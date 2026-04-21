94469ec15fc79babbd09fde46ea838bc64bb62e5
Aleksandr Abramov <ttpo100ajiex@mail.ru>
2026-04-21 18:32:17 +0300

    fix

--- lazyfca/dataset.py
+++ lazyfca/dataset.py
@@ -26,9 +26,8 @@ class Sample:
 
 class Subset:
     def __init__(self, X: pandas.DataFrame, bool_columns: typing.List[str], numeric_columns: typing.List[str]):
-        # asfortranarray is important for covers() to work efficiently and hit the CPU cache
-        self.binary = numpy.asfortranarray(X[bool_columns].to_numpy().astype(bool))
-        self.numeric = numpy.asfortranarray(X[numeric_columns].to_numpy().astype(numpy.float64))
+        self.binary = numpy.ascontiguousarray(X[bool_columns].to_numpy().astype(bool))
+        self.numeric = numpy.ascontiguousarray(X[numeric_columns].to_numpy().astype(numpy.float64))
 
     def __iter__(self):
         for binary, numeric in zip(self.binary, self.numeric):
